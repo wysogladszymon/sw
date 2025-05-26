8eaec994bd8c9b3589d601fa09ce6df3880eefdd
wysogladszymon <wysogladszymon@gmail.com>
2025-05-26 21:05:59 +0200

    Quote updates.
    .

--- frontend/src/data/bookData.ts
+++ frontend/src/data/bookData.ts
@@ -3,11 +3,12 @@ import animal_pharm  from "../assets/books/animal_pharm.jpg";
 import laws_of_power  from "../assets/books/laws_of_power.jpg";
 import pride_and_prejudice  from "../assets/books/pride_and_prejudice.jpg";
 import the_republic  from "../assets/books/the_republic.jpg";
-
+import the_human_bug from "../assets/books/the_human_bug.jpg"
 
 export const bookUrls :string[] = [
   the_republic, 
   meditations,
+  the_human_bug,
   animal_pharm,
   laws_of_power,
   pride_and_prejudice
--- frontend/src/data/quoteData.ts
+++ frontend/src/data/quoteData.ts
@@ -1,34 +1,68 @@
 export interface QuoteInterface {
-  author: string,
-  content: string,
-  contentPL?: string,
+  author: string;
+  content: string;
+  contentPL?: string;
 }
 
 export const quotes: QuoteInterface[] = [
   {
     author: "Marcus Aurelius",
-    content: "Do not be angry at the course of events; they care nothing for you.",
-    contentPL: "Nie należy się gniewać na bieg wypadków. Nic ich to bowiem nie obchodzi."
+    content:
+      "Do not be angry at the course of events; they care nothing for you.",
+    contentPL:
+      "Nie należy się gniewać na bieg wypadków. Nic ich to bowiem nie obchodzi.",
   },
   {
     author: "Marcus Aurelius",
     content: "Wherever one can live, one can live well.",
-    contentPL: "Gdzie można żyć, tam można żyć dobrze."
+    contentPL: "Gdzie można żyć, tam można żyć dobrze.",
   },
   {
     author: "Marcus Aurelius",
-    content: "Waste no more time arguing about what a good man should be. Be one.",
-    contentPL: "",
+    content:
+      "Waste no more time arguing about what a good man should be. Be one.",
+    contentPL:
+      "Nie trać więcej czasu na spory o to, jakim powinien być dobry człowiek. Bądź nim.",
   },
   {
     author: "Marcus Aurelius",
-    content: "The earth likes rain, the high ether likes it too. The universe likes to do what is meant to happen. So I say to the universe: I like what you like.",
-    contentPL: "Lubi deszcz ziemia, lubi wysoki eter”. Wszechświat lubi to robić, co się stać ma. Mówię więc do wszechświata: Lubię to, co ty.",
+    content:
+      "The earth likes rain, the high ether likes it too. The universe likes to do what is meant to happen. So I say to the universe: I like what you like.",
+    contentPL:
+      "Lubi deszcz ziemia, lubi wysoki eter. Wszechświat lubi to robić, co się stać ma. Mówię więc do wszechświata: Lubię to, co ty.",
   },
   {
     author: "Marcus Aurelius",
-    content: "People exist for one another. Either instruct them or bear with them.",
-    contentPL: "Ludzie istnieją dla siebie wzajemnie. Albo ich więc pouczaj, albo znoś.",
+    content:
+      "People exist for one another. Either instruct them or bear with them.",
+    contentPL:
+      "Ludzie istnieją dla siebie wzajemnie. Albo ich więc pouczaj, albo znoś.",
+  },
+  {
+    author: "Marcus Aurelius",
+    content:
+      "Do not worry about the future. You will meet it, if need be, with the same reason that arms you against present ills.",
+    contentPL:
+      "Nie niepokój się o przyszłość. Staniesz bowiem tam, gdy będzie potrzeba, uzbrojony w ten sam rozum, którego zażywasz wobec wypadków bieżących.",
+  },
+  {
+    author: "Marcus Aurelius",
+    content: "Even if you burst, people will go on behaving as they do.",
+    contentPL: "A choćbyś pękł, nie zmieni się postępowanie ludzi.",
+  },
+  {
+    author: "Marcus Aurelius",
+    content:
+      "Do not hanker after what you do not have. Instead, value the most precious of what you do have, and be careful not to become so attached to it that its loss would trouble you.",
+    contentPL:
+      "Nie myśl o tym czego nie masz (...) Ale z tego, co masz, wybierz to, co najbardziej jest cenne (...) Ale uważaj, byś w skutek upodobania w tej rzeczy nie przyzwyczaił się jej tak wysoko cenić, iżbyś miał doznać niepokoju duszy, gdyby ci jej zabrakło.",
+  },
+  {
+    author: "Marcus Aurelius",
+    content:
+      "From Rusticus I learned to love the craft of self-improvement rather than the display of philosophy.",
+    contentPL:
+      "Od Rustyka nauczyłem się, że powinienem pragnąć się doskonalić, a nie pokazywać jako filozof.",
   },
   {
     author: "Antoine de Saint-Exupéry - The Little Prince",
@@ -37,8 +71,10 @@ export const quotes: QuoteInterface[] = [
   },
   {
     author: "Plato",
-    content: "Wise men speak because they have something to say; fools because they have to say something.",
-    contentPL: "Mądrzy mówią, bo mają coś do powiedzenia; głupcy, bo muszą coś powiedzieć.",
+    content:
+      "Wise men speak because they have something to say; fools because they have to say something.",
+    contentPL:
+      "Mądrzy mówią, bo mają coś do powiedzenia; głupcy, bo muszą coś powiedzieć.",
   },
   {
     author: "Plato",
@@ -47,13 +83,17 @@ export const quotes: QuoteInterface[] = [
   },
   {
     author: "Socrates",
-    content: "Prefer knowledge to wealth, for the one is transitory, the other perpetual.",
-    contentPL: "Wybieraj wiedzę zamiast bogactwa, bo jedno jest przemijające, drugie wieczne.",
+    content:
+      "Prefer knowledge to wealth, for the one is transitory, the other perpetual.",
+    contentPL:
+      "Wybieraj wiedzę zamiast bogactwa, bo jedno jest przemijające, drugie wieczne.",
   },
   {
     author: "Socrates",
-    content: "Strong minds discuss ideas, average minds discuss events, weak minds discuss people.",
-    contentPL: "Silne umysły dyskutują o ideach, przeciętne umysły o wydarzeniach, słabe umysły o ludziach.",
+    content:
+      "Strong minds discuss ideas, average minds discuss events, weak minds discuss people.",
+    contentPL:
+      "Silne umysły dyskutują o ideach, przeciętne umysły o wydarzeniach, słabe umysły o ludziach.",
   },
   {
     author: "Robert Greene",
@@ -62,62 +102,118 @@ export const quotes: QuoteInterface[] = [
   },
   {
     author: "Robert Greene",
-    content: "Words have that insidious ability to be interpreted according to the other person’s mood and insecurities.",
-    contentPL: "Słowa mają tę podstępną zdolność do interpretacji w zależności od nastroju i niepewności drugiej osoby.",
+    content:
+      "Words have that insidious ability to be interpreted according to the other person’s mood and insecurities.",
+    contentPL:
+      "Słowa mają tę podstępną zdolność do interpretacji w zależności od nastroju i niepewności drugiej osoby.",
   },
   {
     author: "Robert Greene",
-    content: "There is nothing more intoxicating than victory, and nothing more dangerous.",
-    contentPL: "Nie ma nic bardziej upajającego niż zwycięstwo i nic bardziej niebezpiecznego.",
+    content:
+      "There is nothing more intoxicating than victory, and nothing more dangerous.",
+    contentPL:
+      "Nie ma nic bardziej upajającego niż zwycięstwo i nic bardziej niebezpiecznego.",
   },
   {
     author: "Rocky Balboa",
-    content: "It ain't about how hard you hit. It's about how hard you can get hit and keep moving forward.",
-    contentPL: "Nie chodzi o to jak mocno uderzasz. Chodzi o to jak mocno możesz zostać uderzony i iść dalej.",
+    content:
+      "It ain't about how hard you hit. It's about how hard you can get hit and keep moving forward.",
+    contentPL:
+      "Nie chodzi o to jak mocno uderzasz. Chodzi o to jak mocno możesz zostać uderzony i iść dalej.",
   },
   {
     author: "Rocky Balboa",
     content: "Every champion was once a contender who refused to give up.",
-    contentPL: "Każdy mistrz był kiedyś pretendentem, który odmówił poddania się.",
+    contentPL:
+      "Każdy mistrz był kiedyś pretendentem, który odmówił poddania się.",
   },
   {
     author: "Rocky Balboa",
-    content: "Going in one more round when you don’t think you can – that’s what makes all the difference in your life.",
-    contentPL: "Wejście w jedną rundę więcej, gdy nie myślisz, że możesz - to właśnie robi różnicę w twoim życiu.",
+    content:
+      "Going in one more round when you don’t think you can – that’s what makes all the difference in your life.",
+    contentPL:
+      "Wejście w jedną rundę więcej, gdy nie myślisz, że możesz - to właśnie robi różnicę w twoim życiu.",
   },
   {
     author: "Anthony De Mello",
-    content: "Happiness is like a butterfly. Chase after it and it escapes you; sit down and it settles on your shoulder.",
-    contentPL: "Szczęście jest jak motyl. Gonić za nim, a ucieka; usiądź, a usiądzie na twoim ramieniu.",
+    content:
+      "Happiness is like a butterfly. Chase after it and it escapes you; sit down and it settles on your shoulder.",
+    contentPL:
+      "Szczęście jest jak motyl. Gonić za nim, a ucieka; usiądź, a usiądzie na twoim ramieniu.",
   },
   {
     author: "Harvey Specter",
-    content:"I don’t get lucky. I make my own luck."
+    content: "I don’t get lucky. I make my own luck.",
+    contentPL: "",
   },
   {
     author: "Harvey Specter",
-    content:"Winners don't make excuses when the other side plays the game."
+    content: "Winners don't make excuses when the other side plays the game.",
+    contentPL: "",
   },
   {
     author: "Harvey Specter",
-    content:"You don’t send a puppy to clean up its own mess."
+    content: "You don’t send a puppy to clean up its own mess.",
+    contentPL: "",
   },
   {
     author: "Dr Gregory House, M.D.",
-    content:"You gotta make things better. You can't just keep talking and hope for the best."
+    content:
+      "You gotta make things better. You can't just keep talking and hope for the best.",
+    contentPL: "",
   },
   {
     author: "Dr Gregory House, M.D.",
-    content:"Arrogance has to be earned. Tell what you've done to earn yours."
+    content: "Arrogance has to be earned. Tell what you've done to earn yours.",
+    contentPL: "",
   },
   {
     author: "Piotr Zając (my hero)",
     content: "It was supposed to be as usual, and it turned out as usual.",
-    contentPL:"Miało być jak zwykle, a wyszło jak zwykle."
+    contentPL: "Miało być jak zwykle, a wyszło jak zwykle.",
   },
   {
-    author: "Lord Huron",
-    content: "I had all and then most of you, some, and now none of you.",
+    author: "Cristiano Ronaldo",
+    content: "Of course talent without work is nothing.",
+    contentPL: "",
+  },
+  {
+    author: "Cristiano Ronaldo",
+    content: "I don't need other players to push myself.",
+    contentPL: "",
+  },
+  {
+    author: "Niccolò Machiavelli",
+    content: "The wise man does at once what the fool does finally.",
+    contentPL:
+      "Mądry robi od razu to, co głupiec zmuszony będzie zrobić dopiero na koniec.",
+  },
+  {
+    author: "Niccolò Machiavelli",
+    content:
+      "Men will always be bad for you, unless necessity makes them good.",
+    contentPL:
+      "Ludzie będą zawsze dla ciebie źli, jeżeli konieczność nie zmusi ich do tego, by byli dobrzy.",
+  },
+  {
+    author: "Sun Tzu",
+    content: "He who desires peace but sets no conditions prepares a trap.",
+    contentPL:
+      "Kto pragnie pokoju, a nie stawia żadnych warunków, przygotowuje podstęp.",
+  },
+  {
+    author: "Sun Tzu",
+    content:
+      "To win one hundred victories in one hundred battles is not the acme of skill. To subdue the enemy without fighting is the acme of skill.",
+    contentPL:
+      "Osiągnąć sto zwycięstw w stu bitwach nie jest szczytem umiejętności. Szczytem umiejętności jest pokonanie przeciwnika bez walki.",
+  },
+  {
+    author: "Albert Einstein",
+    content:
+      "Insanity is doing the same thing over and over again and expecting different results.",
+    contentPL:
+      "Szaleństwem jest robić wciąż to samo i oczekiwać innych rezultatów.",
   },
 ];
 
@@ -132,7 +228,8 @@ function shuffle(array: object[]) {
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]
+      array[randomIndex],
+      array[currentIndex],
     ];
   }
 }
